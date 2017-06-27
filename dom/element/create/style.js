d4555f85e573525c4bfecc166d6777dfb92381de
Yurk Sha <yurkins@gmail.com>
2017-06-27 16:33:10 +0300

    dom.element.create.style: remote load

--- dom/element/create/style.js
+++ dom/element/create/style.js
@@ -3,13 +3,19 @@ var get = require('../get');
 var prependEl = require('../prepend');
 var createEl = require('../create');
 var head = document.head || get('head');
+var re_isUrl = /.css$/;
 
 module.exports = function(css, append, className) {
-    var style = createEl('style', {type: 'text/css', className: className || ''});
+    var isRemote = re_isUrl.test(css);
+    var attrs = {type: 'text/css', className: className || ''};
+    if (isRemote) {
+        attrs.rel = 'stylesheet';
+    }
+    var style = createEl(isRemote ? 'link' : 'style', attrs);
     if (append) {
         prependEl(style, append === true ? head : append);
     }
-    if (css) {
+    if (css && !isRemote) {
         if (style.styleSheet && !style.sheet) {
             style.styleSheet.cssText = css;
         } else {
